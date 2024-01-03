a2741774ea0e7ca67a550b54cca7bb45284fd8b8
mayuri-hostnextra <mayuri@hostnextra.com>
2024-01-03 11:17:30 +0530

    Inital Commit

---
+++ src/pack1/D.java
@@ -0,0 +1,18 @@
+package pack1;
+
+//import p1.Student;
+import static p1.Student.*;
+
+public class D {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Student s = new Student(); System.out.println(s.fees);
+		 */
+		
+		System.out.println(fees);
+		System.out.println("Fees = "+getFees());
+	}
+	
+}
